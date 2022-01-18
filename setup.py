f4ca61c2bf1b063faa874a470f824283a347c7a1
Romain Rouphael <romainrouphael@gmail.com>
2022-01-18 10:14:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,8 @@
 #!/usr/bin/env python
 from setuptools import setup
-import codecs
 import os
 import re
 
-
-with codecs.open(
-        os.path.join(
-            os.path.abspath(os.path.dirname(__file__)),
-            'lnmarkets',
-            '__init__.py'
-        ), 'r', 'latin1') as fp:
-    try:
-        version = re.findall(r"^__version__ = '([^']+)'\r?$", fp.read(), re.M)[0]
-    except IndexError:
-        raise RuntimeError('Unable to determine version.')
 
 with open("README.rst", "r") as fh:
     long_description = fh.read()
