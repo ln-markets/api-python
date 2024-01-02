367878540af88c97e0812462f7c152439bfb9123
Romain Rouphael <romain.rouphael@ito.am>
2024-01-02 16:17:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='ln-markets',
-    version='2.0.4',
+    version='2.0.5',
     packages=['lnmarkets'],
     description='LN Markets API python implementation',
     long_description=long_description,
